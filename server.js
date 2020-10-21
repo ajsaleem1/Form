956e9b9ef02cc822ffb2a94d8f71a017e08ba55c
abdul saleem <ajsaleem6@gmail.com>
2020-10-21 17:56:29 +0530

    new

--- server.js
+++ server.js
@@ -96,11 +96,14 @@ app.post(
     }
 );
 
-db.sequelize
-    .sync()
-    .then(() => {
-        app.listen(8080, () => {
-            console.log(`started on port 8080`);
-        });
-    })
-    .catch((err) => console.log(err));
+// Use the environment variable or use a given port
+const PORT = process.env.PORT || 8080;
+
+// Create a server, uses `handleRequest` which is function that takes
+// care of providing requested data
+const server = http.createServer(handleRequest);
+
+// Start the server
+server.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:%s', PORT);
+});
